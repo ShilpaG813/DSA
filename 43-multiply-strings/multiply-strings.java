9f87da802aa96553fbc832eaffacd46f70970567
Shilpa G <1ms24cs415@msrit.edu>
2026-08-23 20:07:37 +0530

    Time: 3 ms (78.99%) | Memory: 43.5 MB (80.91%) - LeetSync

---
+++ 43-multiply-strings/multiply-strings.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String multiply(String num1, String num2) {
+        if(num1.equals("0") || num2.equals("0")) return "0";
+        int m=num1.length();
+        int n=num2.length();
+        int res[]=new int[m+n];
+        for(int i=m-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                int a=num1.charAt(i)-'0';
+                int b=num2.charAt(j)-'0';
+                int sum=a*b+res[i+j+1];
+                res[i+j]+=sum/10;
+                res[i+j+1]=sum%10;
+            }
+        }
+        StringBuilder sb=new StringBuilder();
+        for(int num:res){
+            if(num==0 && sb.length()==0){
+                continue;
+            }
+            sb.append(num);
+        }return sb.toString();
+    }
+}
